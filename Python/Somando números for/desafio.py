9e051c88a1160b03f3c56b9edd9e2354a47db5a8
Leozindev_genius <leonardohenriquefr785@gmail.com>
2025-01-10 10:00:12 -0300

    desafio feito

--- Python/Somando números for/desafio.py
+++ Python/Somando números for/desafio.py
@@ -3,18 +3,21 @@
 # Se o usuário digitar 5, o programa deve somar 1 + 2 + 3 + 4 + 5 e exibir o resultado final.
 
 
-# Não foi concluído ainda
 
 def Soma_num():
     while True:
         try:
             num = int(input('Digite um número inteiro: '))
-        
+
         except ValueError:
-            print('\033[31mDigite um número inteiro valido! Tente novamente.\033[m')
+            print('\033[31mDigite um número inteiro valido! Tente novamente\033[m')
             continue
 
+        soma = 0    
+        for i in range(1, num + 1):
+            soma += i
         
+        print(f'Somando 1 até {num}: temos {soma}')
+
 
 Soma_num()
- 
