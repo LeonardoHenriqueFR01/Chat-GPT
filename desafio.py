e24e9c9b09094e8c220fad4ca42da9d59d65b156
Leozindev_genius <leonardohenriquefr785@gmail.com>
2025-02-05 16:41:23 -0300

    js commit

--- desafio.py
+++ desafio.py
@@ -1,3 +1,4 @@
+# Tipos de triângulos
 
 
 def ladostriangulo():
@@ -7,20 +8,20 @@ def ladostriangulo():
             n2 = int(input('[+] Digite o 2° lado: '))
             n3 = int(input('[+] Digite o 3° lado: '))
 
-            if (n1 == n2) and (n1 == n3) and (n2 == n3):
-                print('É um Equilátero.')
+            # Verificando se os lados formam um triângulo
+            if (n1 + n2 > n3) and (n1 + n3 > n2) and (n2 + n3 > n1):
+                # Classificação do triângulo
+                if n1 == n2 == n3:
+                    print('É um Equilátero.')
+                elif n1 == n2 or n1 == n3 or n2 == n3:
+                    print('É um Isósceles.')
+                else:
+                    print('É um Escaleno.')
                 break
+            else:
+                print('[!] Esses valores não formam um triângulo. Tente novamente.')
 
-            elif (n1 == n2) and (n1 != n3):
-                print('É um Esósceles.')
-                break
-            
-            elif (n1 != n2) and (n1 != n3) and (n2 != n3):
-                print('É um Escaleno') 
-                break
-        
         except ValueError:
-            print('[+] Digite um número inteiro valido! Tente novamente.')
-            continue
+            print('[+] Digite um número inteiro válido! Tente novamente.')
 
 ladostriangulo()
